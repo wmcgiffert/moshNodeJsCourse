f4e2d312003c001a28d8c4fc30bdd78f32e1d54b
William McGiffert <William.McGiffert@alliancebernstein.com>
2022-06-15 16:22:13 -0500

    Built In Modules

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-var modules = require('./modules');
-console.log(modules);
-modules('used log function'); 
+const os = require('os');
+
+var osObj = os.totalmem();
+var osObj2 = os.freemem();
+console.log(`Total Memory: ${osObj} \n Free Memory: ${osObj2}`);
+
+
+// const path = require('path');
+
+// var pathObj = path.parse(__filename); 
+// console.log(pathObj);
+
+// var modules = require('./modules');
+// console.log(modules);
+// modules('used log function'); 
