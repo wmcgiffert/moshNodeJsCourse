94270cf62f9063f3fb199e9d25e248c3c137ae25
wmcgiffert <wmcgiffert@crimson.ua.edu>
2022-06-16 17:48:01 -0500

    eventArguments

---
+++ app.js
@@ -0,0 +1,12 @@
+const { EventEmitter } = require('events');
+const emitter = new EventEmitter();
+
+//Register a listener *Register must come first* if it was after the Signaler would have signaled to nothing  
+emitter.on('messageLogged', function(arg){
+    console.log(arg);
+});
+
+//Signalling that an event has happened
+//Raised an event but has to have a emitter to listen
+emitter.emit('messageLogged', { id: 1, url: 'http://' });
+
