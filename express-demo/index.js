2d36b8e53ef8ea3d52e8955feaaf17046827388c
William McGiffert <William.McGiffert@alliancebernstein.com>
2022-07-13 15:40:40 -0500

    installed nodemon

---
+++ express-demo/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send('Hello World');
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send([1, 2, 3]);
+})
+
+
+// app.post();
+// app.delete();
+// app.put();
+app.listen(3000, ()=> console.log('Listening on port 3000'));
