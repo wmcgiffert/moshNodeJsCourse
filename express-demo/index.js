16502957fd7a1ed2ea03e2db11a324d11ec16ba0
William McGiffert <William.McGiffert@alliancebernstein.com>
2022-07-13 16:05:49 -0500

    failed to install nodemon

--- express-demo/index.js
+++ express-demo/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send('Hello World');
+    res.send('Hello World!!!!!!!');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send([1, 2, 3, 4]);
 })
 
 
