459f0ad1e7ee25a763e821adcddca3d39f6365a9
wmcgiffert <wmcgiffert@crimson.ua.edu>
2022-07-14 19:22:16 -0500

    first express url paths

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const app = express();
+const courses = [
+    {
+        id: 1,
+        name: 'Information Sciences'
+    },
+    {
+        id: 2,
+        name: 'MAcc'
+    },
+    {
+        id: 3,
+        name: 'Accounting'
+    },
+]
 
 app.get('/', (req,res) => {
     res.send('Hello World!!!!!!!');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
+})
+
+app.get('/api/courses/:id', (req,res) => {
+    let course = courses.find(c=> c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The course with the given id was not found');
+    res.send(course);
 })
 
 
@@ -15,6 +35,5 @@ app.get('/api/courses', (req, res) => {
 // app.put();
 
 //PORT
-const portNum = 3000;
-process.env.PORT || portNum;
+const portNum = process.env.PORT || 3000;
 app.listen(portNum, ()=> console.log(`Listening on port ${portNum}...`));
