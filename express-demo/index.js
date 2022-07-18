dccd75f3d011b23c4454eb214195f19622db298f
wmcgiffert <wmcgiffert@crimson.ua.edu>
2022-07-18 10:48:32 -0500

    post request

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+
+app.use(express.json()); 
+
 const courses = [
     {
         id: 1,
@@ -23,16 +26,21 @@ app.get('/api/courses', (req, res) => {
     res.send(courses);
 })
 
+app.post('/api/courses', (req,res) => {
+    const course = {
+        id: courses.length + 1, 
+        name: req.body.name,
+    };
+    courses.push(course);
+    res.send(courses); 
+})
+
 app.get('/api/courses/:id', (req,res) => {
     let course = courses.find(c=> c.id === parseInt(req.params.id));
     if(!course) res.status(404).send('The course with the given id was not found');
     res.send(course);
 })
 
-
-// app.post();
-// app.delete();
-// app.put();
 
 //PORT
 const portNum = process.env.PORT || 3000;
