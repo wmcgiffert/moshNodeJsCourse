8387e04779453f97b4dd8b10141ad1546f5273bb
William McGiffert <William.McGiffert@alliancebernstein.com>
2022-08-29 16:10:08 -0500

    updated vidly app to relocate genres routes to a genres file

--- ExpressApp/index.js
+++ ExpressApp/index.js
@@ -1,90 +1,13 @@
 const Joi = require('joi');
 const express = require('express');
 const app = express();
+const genres = require('./routes/genres');
+
+
 
 app.use(express.json()); 
+app.use('/api/genres', genres);
 
-
-const genres = [
-    {
-        id:1,"genre":"horror"
-    },
-    {
-        id:2,"genre":"action"
-    },
-    {
-        id:3,"genre":"comedy"
-    }
-]
-
-//Get
-//Get all the genres
-app.get('/api/genres', (req,res)=>{
-    res.send(genres);
-})
-
-//Get a single genre
-app.get('/api/genres/:id', (req, res)=>{
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
-    if(!genre) return req.status(404).send('Could not locate a genre with {0} id', req.params);
-
-    console.log(genre);
-    res.send(genre);
-})
-
-//Post routes
-//Post a new genre
-app.post('/api/genres', (req, res) =>{
-    const { error } = validateCourse(req.body);
-    if(error) return res.status(404).send('Please check the object structure.');
-
-    const genre = {
-        id: genres.length + 1,
-        genre: req.body.genre
-    };
-
-    genres.push(genre);
-    res.send(genres);
-})
-
-//Put routes
-//Edit a single genre
-app.put('/api/genres/:id', (req, res) =>{
-    let genre = genres.find(g => g.id === parseInt(req.params.id));
-    const id = req.params.id;
-    if(!genre) return res.status(404).send(`Could not locate a genre with ${id} id`);
-    
-    
-    const { error } = validateCourse(req.body);
-    if(error) return res.status(404).send('Invalid genre object please input valid course object')
-
-    genre.genre = req.body.genre; 
-  
-    res.send(genres); 
-})
-
-//Delete routs
-//Delete a single genre
-app.delete('/api/genres/:id', (req,res)=>{
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
-    if(!genre) return res.status(404).send('The genre with the given ID was not found'); 
-    
-    const index = genres.indexOf(genre); 
-
-    genres.splice(index,1);
-
-    res.send(genres);
-})
-
-
-
-//private/public functions
-function validateCourse(genre){
-    const schema = Joi.object({
-        genre: Joi.string().min(3).required()
-    });
-    return result = schema.validate(genre);
-}
 
 const portNum =process.env.PORT || 3000;
 app.listen(portNum, ()=> console.log(`Listening on port ${portNum}.....`)); 
---
+++ ExpressApp/routes/genres.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const router = express.Router();
+const Joi = require('joi');
+
+const genres = [
+    {
+        id:1,"genre":"horror"
+    },
+    {
+        id:2,"genre":"action"
+    },
+    {
+        id:3,"genre":"comedy"
+    }
+]
+
+//Get
+//Get all the genres
+router.get('/', (req,res)=>{
+    res.send(genres);
+})
+
+//Get a single genre
+router.get('/:id', (req, res)=>{
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
+    if(!genre) return req.status(404).send('Could not locate a genre with {0} id', req.params);
+
+    console.log(genre);
+    res.send(genre);
+})
+
+//Post routes
+//Post a new genre
+router.post('/', (req, res) =>{
+    const { error } = validateCourse(req.body);
+    if(error) return res.status(404).send('Please check the object structure.');
+
+    const genre = {
+        id: genres.length + 1,
+        genre: req.body.genre
+    };
+
+    genres.push(genre);
+    res.send(genres);
+})
+
+//Put routes
+//Edit a single genre
+router.put('/:id', (req, res) =>{
+    let genre = genres.find(g => g.id === parseInt(req.params.id));
+    const id = req.params.id;
+    if(!genre) return res.status(404).send(`Could not locate a genre with ${id} id`);
+    
+    
+    const { error } = validateCourse(req.body);
+    if(error) return res.status(404).send('Invalid genre object please input valid course object')
+
+    genre.genre = req.body.genre; 
+  
+    res.send(genres); 
+})
+
+//Delete routs
+//Delete a single genre
+router.delete('/:id', (req,res)=>{
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
+    if(!genre) return res.status(404).send('The genre with the given ID was not found'); 
+    
+    const index = genres.indexOf(genre); 
+
+    genres.splice(index,1);
+
+    res.send(genres);
+})
+
+
+
+//private/public functions
+function validateCourse(genre){
+    const schema = Joi.object({
+        genre: Joi.string().min(3).required()
+    });
+    return result = schema.validate(genre);
+}
+
+
+module.exports = router;
