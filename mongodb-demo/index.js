b62d08f7f336d70da1cb93b73cc1d7332a20e87d
William Garrett McGiffert <williamgarrettmcgiffert@64.124.115.22.idia-282912-zyo.zip.zayo.com>
2022-09-08 14:09:08 -0500

    mongodb added a course

---
+++ mongodb-demo/index.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+    .then(() => console.log('Connected to MongoDB....'))
+    .catch(err => console.error('Could not connect to MongoDb', err));
+
+const cousreSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: {type: Date, default: Date.now},
+    isPublished: Boolean
+})
+
+const Course = mongoose.model('Course', cousreSchema);
+
+async function createCourse(){
+    
+    const course = new Course({
+        name: 'Mosh Node.js Course',
+        author: 'Garrett',
+        tags: ['node', 'backend'],
+        isPublished: true
+    });
+
+    const result = await course.save();
+    console.log(result);
+}
+
+createCourse();
