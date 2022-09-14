4d5d43bd0e8626ffcf18a3ad96f40a7efbb9bc24
William Garrett McGiffert <williamgarrettmcgiffert@Williams-Mac-mini.local>
2022-09-14 12:04:18 -0500

    Mongo Query words added

--- mongodb-demo/index.js
+++ mongodb-demo/index.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose');
 
+// Mongo filter words
+/* 
+eq (equal)
+ne (not equal)
+gt (greater than)
+gte (greater or than equal to)
+lt (less than)
+lte (less than or equal to)
+in (in)
+nin (not in)
+
+
+*/
+
+
 mongoose.connect('mongodb+srv://wgmMoshApp:wgmMoshApp@mernmoshapp.bxgpy2d.mongodb.net/?retryWrites=true&w=majority')
     .then(() => console.log('Connected to MongoDB....'))
     .catch(err => console.error('Could not connect to MongoDb', err));
-
 
 const courseSchema = new mongoose.Schema({
     name: String,
@@ -16,7 +30,6 @@ const courseSchema = new mongoose.Schema({
 const Course = mongoose.model('Course', courseSchema);
 
 async function createCourse(){
-    
     const course = new Course({
         name: 'Mosh Node.js Course',
         author: 'Garrett',
@@ -29,9 +42,13 @@ async function createCourse(){
 }
 
 async function getCourses(){
-    const courses = await Course.find({author: 'Garrett', isPublished: true, tags: 'backend'});
+    const courses = await Course
+        .find({author: 'Garrett', isPublished: true})
+        .limit(3)
+        .sort({name:1})
+        .select({name:1, tags:1});
     console.log(courses);
 }
 
-getCourses();
+// getCourses();
 // createCourse();  
